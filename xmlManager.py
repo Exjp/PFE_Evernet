2701c856b049846ba8072be9e1eadf2ca94a40f8
glartigue <ghislain.lartigue@etu.u-bordeaux.fr>
2021-03-06 17:22:24 +0100

    rework de la bdd

--- xmlManager.py
+++ xmlManager.py
@@ -33,17 +33,15 @@ def aliasUnique(aliasValue):
 def numberUnique(numberValue):
     unique = True
     for elem in root:
-        for var in elem:
-            if var.text == numberValue:
-                 unique = False
+        if elem.attrib['number'] == numberValue:
+            unique = False
     return unique
 
 def keyUnique(keyValue):
     unique = True
     for elem in root:
-        for var in elem:
-            if var.text == keyValue:
-                unique = False
+        if elem.attrib['key'] == keyValue:
+            unique = False
     return unique
 
 
@@ -55,26 +53,17 @@ def addUser(aliasValue, passValue, numberValue, keyValue):
         user = ET.Element('user')
         user.set("alias", aliasValue)
         user.set("banned", "False")
-        
-        password = ET.SubElement(user, "password")
+
         salt = bcrypt.gensalt()
         hashed_password = bcrypt.hashpw(passValue.encode('utf-8'), salt)
-        password.text = hashed_password.decode('utf8')
-        
-        number = ET.SubElement(user, "number")
-        number.text = numberValue
-
-        key = ET.SubElement(user, "key")
-        key.text = keyValue
+        user.set("password", hashed_password.decode('utf8'))
+        user.set("number", numberValue)
+        user.set("key", keyValue)
 
         root.append(user)
         treeWrite()
     else :
         print("User already exists")
-
-
-def removeUserFromNumber(number):
-    treeWrite()
 
 
 # return un erreur si pas trouvé, nullptr, verif le nom en entrée
@@ -84,15 +73,18 @@ def removeUserFromName(name):
             root.remove(elem)
     treeWrite()
 
+def removeUserFromNumber(number):
+    for elem in root:
+        if elem.attrib['number'] == number:
+            root.remove(elem)
+    treeWrite()
 
 # return une info si utilisateur banni
 def login(alias, password):
     for elem in root:
         if elem.attrib['alias'] == alias:
-            for var in elem:
-                if var.tag == "password":
-                    if bcrypt.checkpw(password.encode('utf8'), (var.text).encode()):
-                        return True
+            if bcrypt.checkpw(password.encode('utf8'), elem.attrib['password'].encode()):
+                return True
     return False
 
 
@@ -113,61 +105,58 @@ def unBanUser(alias):
 # erreur si existe pas ?
 def isBanned(alias):
     for elem in root:
-        if elem.attrib['alias'] == alias and elem.attrib['banned'] == "False":
+        if elem.attrib['alias'] == alias and elem.attrib['banned'] == "True":
             return True
     return False
+
+def exists(alias):
+    for elem in root:
+        if elem.attrib['alias'] == alias:
+            return True
+    return False
+
 
 # verif que les user soient pas ban avant de les renvoyer
 # verifier les noms en entrée (nullptr, trop gros, non utf8)
 def getNumberFromAlias(name):
     for elem in root:
         if elem.attrib['alias'] == name and elem.attrib['banned'] == "False":
-            for var in elem:
-                if var.tag == "number":
-                    return var.text
+            return elem.attrib['number']
     return "Error : alias not found"
 
 # verifier les numeros en entrée
 def getAliasFromNumber(number):
     for elem in root:
-        for var in elem:
-            if var.tag == "number" and var.text == number:
-                return elem.attrib['alias']
+        if elem.attrib['number'] == number and elem.attrib['banned'] == "False":
+            return elem.attrib['alias']
     return "Error : number not found"
 
 #verifier les noms en entrée (nullptr, trop gros, non utf8)
 def getKeyFromAlias(name):
     for elem in root:
         if elem.attrib['alias'] == name and elem.attrib['banned'] == "False":
-            for var in elem:
-                if var.tag == "key":
-                    return var.text
+            return elem.attrib['key']
     return "Error : alias not found"
 
 
 #return une liste des alias dans la bdd, erreur si y'a personne dans la bdd
 #pas teste
 def getAliases():
-    return [elem.attrib['alias'] for elem in root if elem.attrib[banned]=="False"]
+    return [elem.attrib['alias'] for elem in root if elem.attrib['banned']=="False"]
 
 
-#pas teste, explose si le sender est ban
+#erreur si le sender est ban (try catch avec isBanned(sender) / erreur si le sender existe pas
 def randomUsers(num,sender):
     listAlias = getAliases()
     listAlias.pop(listAlias.index(sender))
 
-    for elem in root:
-        if elem.attrib['banned'] == "True":
-            listAlias.pop(listAlias.index(elem.attrib['alias']))
-
     num = int(num)
     sizeListAlias = len(listAlias)
-    tmpList = [[0 for x in range(num)] for y in range(2)]
+    tmpList = [[0 for x in range(2)] for y in range(num)]
 
 
     if num > sizeListAlias:
-        print("nombre demandé trop grand")
-        return
+        return "Not enough numbers in database..."
 
     cnt=0
     aleaIndList = random.sample(range(sizeListAlias), num)
@@ -188,8 +177,7 @@ def verifyInvitationKey(invitation_key):
 
 
 def main():
-    banUser("Roger")
-    x = getNumberFromAlias("Roger")
+    x = randomUsers(4, "Roger")
     print(x)
 
 if __name__ == "__main__":
