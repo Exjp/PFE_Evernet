32313c8824cb44e5eee0e2ec7918094f5819a5db
Jeremy <jchardon@u-bordeaux.fr>
2021-03-11 22:39:23 +0100

    modif test perf

--- execTest.py
+++ execTest.py
@@ -1,4 +1,4 @@
-import multiprocessing, os
+import multiprocessing, os, time
 
 def worker(file):
     os.system(file)
@@ -6,6 +6,10 @@ def worker(file):
 
 if __name__ == '__main__':
     num = 10
-    for i in range(80):
+    for i in range(5000):
+        #for i in range(1):
         p = multiprocessing.Process(target=worker, args=("Client2.py",))
         p.start()
+        print(i)
+        time.sleep(0.2)
+    exit()
