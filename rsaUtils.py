f2eb33aee4ac814ff6c234d9df44ebc512eb92aa
Mr-Clem <clem.cn37@gmail.com>
2021-03-12 11:35:41 +0100

    add functions to rsaUtils and test passed

--- rsaUtils.py
+++ rsaUtils.py
@@ -2,8 +2,9 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_v1_5 as cipher_algorithm
 from OpenSSL import crypto
 import base64
+import xmlManager
 
-def encrypt(data, pseudo):
+def encrypt_with_pem(data, pseudo):
     certPath = pseudo+"_crt.pem"
     str_cert = open(certPath, 'rt').read()
     cert = crypto.load_certificate(crypto.FILETYPE_PEM, str_cert)
@@ -12,10 +13,23 @@ def encrypt(data, pseudo):
     public = RSA.importKey(pKeyString)
 
     cipher = cipher_algorithm.new(public)
-    cipher_text = cipher.encrypt(data)#.encode())
+    cipher_text = cipher.encrypt((data).encode())
     signed = base64.b64encode(cipher_text)
 
-    return sign
+    return signed
+
+def encrypt(data, pseudo):
+    str_cert = xmlManager.getKeyFromAlias(pseudo)
+    cert = crypto.load_certificate(crypto.FILETYPE_PEM, str_cert)
+    pKey = cert.get_pubkey()
+    pKeyString = crypto.dump_publickey(crypto.FILETYPE_PEM,pKey)
+    public = RSA.importKey(pKeyString)
+
+    cipher = cipher_algorithm.new(public)
+    cipher_text = cipher.encrypt((data).encode())
+    signed = base64.b64encode(cipher_text)
+
+    return signed
 
 def decrypt(cipherText):
     keyPath = "ca_key.pem"
@@ -27,6 +41,21 @@ def decrypt(cipherText):
     cipher = base64.b64decode(cipherText)
     pr = cipher_algorithm.new(privat)
     x = pr.decrypt(cipher, "error")
-    #x = x.decode('utf-8')
+    x = x.decode('utf-8')
+
+    return x
+
+def decrypt_with_file(cipherText, pseudo):
+    keyPath = pseudo+"_key.pem"
+    with open(keyPath,'rb') as fk:
+    	priv = fk.read()
+    	fk.close()
+    privat = RSA.importKey(priv)
+
+    cipher = base64.b64decode(cipherText)
+    pr = cipher_algorithm.new(privat)
+    x = pr.decrypt(cipher, "error")
+    print(x)
+    x = x.decode('utf-8')
 
     return x
--- testRsaUtils.py
+++ testRsaUtils.py
@@ -1,11 +1,12 @@
 from rsaUtils import *
 
+#Testing with pem file
 st = "This is a text to test our encrytion and decrytion functions!"
 print(st)
 
 test_counter = 0
 
-enc_str = encrypt(st, 'test_crt.pem')
+enc_str = encrypt_with_pem(st, "ca")
 if(st != enc_str):
     print("Encrypted text:")
     print(enc_str)
@@ -14,7 +15,7 @@ if(st != enc_str):
 else:
     print("---Encryption did not work, encrypted text and original are the same---")
 
-dec_str = decrypt(enc_str, 'test_key.pem')
+dec_str = decrypt(enc_str)
 
 if(st == dec_str):
     print("Decrypted text:")
@@ -24,5 +25,35 @@ if(st == dec_str):
 else:
     print("---Decryption did not work, decrypted text and original are not the same---")
 
+print("")
+print("Now testing with database")
+print("")
 
-print("---Test passed: " + str(test_counter) + "/2---")
+#Testing with database
+xmlManager.init()
+xmlManager.removeUserFromName("test")
+xmlManager.addUser("test", "test", "0987654321", open("ca_crt.pem", 'rt').read())
+
+st = "This is a text to test our encrytion and decrytion functions!"
+print(st)
+
+enc_str = encrypt(st, "test")
+if(st != enc_str):
+    print("Encrypted text:")
+    print(enc_str)
+    print("---Encryption worked well---")
+    test_counter+=1
+else:
+    print("---Encryption did not work, encrypted text and original are the same---")
+
+dec_str = decrypt(enc_str)
+
+if(st == dec_str):
+    print("Decrypted text:")
+    print(dec_str)
+    print("---Decryption worked well---")
+    test_counter+=1
+else:
+    print("---Decryption did not work, decrypted text and original are not the same---")
+
+print("---Test passed: " + str(test_counter) + "/4---")
